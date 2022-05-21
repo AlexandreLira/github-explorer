4f0bd33a941f1a87c19d8e25076ec79706d6a827
Alexandre Lira <alexandrelira112@gmail.com>
2022-05-21 14:40:37 -0300

    fast laoder webpack

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App(){
-    // throw new Error('Eita Giovanna, o forninho caiu!');
     return (
-        <RepositoryList/>
+        <>
+            <RepositoryList/>
+        </>
     )
 }
