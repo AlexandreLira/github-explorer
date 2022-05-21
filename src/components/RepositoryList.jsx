e628a5c48bd4fc69d5cb31e4f8e2d38916fb3684
Alexandre Lira <alexandrelira112@gmail.com>
2022-05-21 16:03:50 -0300

    get repository from github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,31 @@
+import { useEffect, useState } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
-
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/AlexandreLira/tillsapp'
-}
+import '../styles/repositories.scss';
 
 export function RepositoryList(){
+    const [ repositories, setRepositories ] = useState([])
+    useEffect(() => {
+        async function loadRepositories() {
+            const URL = 'https://api.github.com/orgs/rocketseat/repos'
+            const response = await fetch(URL)
+            const data = await  response.json()
+            setRepositories(data.map(item => ({
+                name: item.name,
+                description: item.description,
+                link: item.url,
+            })))
+        }
+        loadRepositories()
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem repository={repository}/>
+                {repositories.map((repository, index) => (
+                        <RepositoryItem repository={repository} key={index}/>
+                ))}
             </ul>
         </section>
     )
